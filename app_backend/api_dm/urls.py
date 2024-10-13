7bb4828ed1c44861c8797aadf3d3df9e7a635097
Kodai <0711dr5@gmail.com>
2024-10-13 19:17:52 +0900

    fix:api_dm ビュー作成

--- app_backend/api_dm/urls.py
+++ app_backend/api_dm/urls.py
@@ -1,11 +1,26 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include  
+from rest_framework.routers import DefaultRouter 
+from api_dm import views 
 
+# アプリケーションの名前空間を定義。これにより、他のアプリケーションとのURL名の衝突を避ける
 app_name = 'dm'
 
+# DefaultRouterのインスタンスを作成
+# DefaultRouterは、ViewSetに基づいて自動的にURLを生成します
 router = DefaultRouter()
 
-urlpatterns = [
-    path('',include(router.urls)),
-]
+# 'message'というパスに対して、MessageViewSetを登録
+# これにより、メッセージ送受信に関連するCRUD（作成、取得、更新、削除）操作が可能になります
+# basename='message' は、URL生成時に使われる名前のベースです
+router.register('message', views.MessageViewSet, basename='message')
 
+# 'inbox'というパスに対して、InboxListViewを登録
+# これにより、受信ボックスに関連する操作（メッセージの表示）を管理できます
+router.register('inbox', views.InboxListView, basename='inbox')
+
+# urlpatternsに定義されたパスとViewを対応させる
+urlpatterns = [
+    # ルーターによって自動生成されたURLパターンをインクルードします
+    # 'message/' と 'inbox/' に関連するURLが自動的に生成され、それぞれのViewSetが対応するリクエストを処理します
+    path('', include(router.urls)),
+]
--- app_backend/api_user/urls.py
+++ app_backend/api_user/urls.py
@@ -1,11 +1,31 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include  # URLパターンを定義するためのモジュールをインポート
+from rest_framework.routers import DefaultRouter  # ViewSetのURL自動生成を行うためのDefaultRouterをインポート
+from api_user import views  # 使用するビューをインポート
 
+# アプリケーションの名前空間を定義。これにより他のアプリケーションとのURL名の衝突を避ける
 app_name = 'user'
 
+# DefaultRouterのインスタンスを作成
+# DefaultRouterは、ViewSetに基づいて自動的にURLを生成します
 router = DefaultRouter()
 
-urlpatterns = [
-    path('',include(router.urls)),
-]
+# 'profile'というパスに対して、ProfileViewSetを登録
+# これにより、プロファイル情報に関するCRUD（作成、取得、更新、削除）操作が可能になります
+router.register('profile', views.ProfileViewSet)
 
+# 'approval'というパスに対して、FriendRequestViewSetを登録
+# これにより、友達リクエストに関するCRUD操作が可能になります
+router.register('approval', views.FriendRequestViewSet)
+
+# urlpatternsに定義されたパスとViewを対応させる
+urlpatterns = [
+    # 'create/' というURLにアクセスすると、新規ユーザー作成用のCreateUserViewが呼び出される
+    path('create/', views.CreateUserView.as_view(), name='create'),
+
+    # 'myprofile/' というURLにアクセスすると、ログイン中のユーザーのプロフィール情報を取得するMyProfileListViewが呼び出される
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
+
+    # それ以外のURLは、DefaultRouterが自動的に生成したURLを含めて処理する
+    # 'profile/' や 'approval/' のパスが自動的に生成され、登録したViewSetがそれぞれ対応します
+    path('', include(router.urls)),
+]
